a30c384b92189102dff8aebbccaaea701f770508
ayushidewda <124859793+ayushidewda@users.noreply.github.com>
2023-07-29 15:58:37 +0530

    Add files via upload

---
+++ Task1-Personal Portfolio/script.js
@@ -0,0 +1,26 @@
+var typed = new Typed(".text",{
+    strings:[" Frontend Developer" , " Photographer" , " Web Developer", " Editor"],
+    typeSpeed:100,
+    backSpeed:10,
+    backDelay:10,
+    loop:true
+
+})
+CSS.registerProperty({
+    name: "--p",
+    syntax: "<integer>",
+    initialValue: 0,
+    inherits: true,
+  });
+// // Smooth scrolling for anchor links
+// const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+// menuLinks.forEach(link => {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const target = document.querySelector(this.getAttribute('href'));
+//     target.scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     });
+//   });
+// });
